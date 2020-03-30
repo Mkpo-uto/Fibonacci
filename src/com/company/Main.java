6145fae50a3381bfb57eebba27a6eea3a5d887b9
mkpou <mkpoutoe@hotmail.com>
2020-03-31 07:49:14 +1100

    recursion fibonacci

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,12 +22,16 @@ public class Main {
         int sumValue = getSum(productValue);
         System.out.println(" Sum is " + sumValue);
 
+        //with recursion
+        int countR = 10;
+        System.out.print(n1 + " " + n2);//printing 0 and 1
+        printFibonacci(countR-2);//n-2 because 2 numbers are already printed
 
     }
 
     public static int getProduct(int n, int c){
 
-        int product = 1,  sum = 0, n3 , i, count = n ;
+        int product = 1,  sum = 0 ;
         while (n > 0) {
             int n1 = n -1, n2 = n - c;
             if(n == 1){
@@ -41,6 +45,16 @@ public class Main {
 
         }
         return sum;
+    }
+    static int n1 = 0, n2 = 1, n3 = 0;
+    public static void printFibonacci(int count){
+        if(count > 0){
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+        System.out.print(" " + n3);
+        printFibonacci(count-1);
+        }
     }
 
     public static int getSum (int prod){
